fb82acdb1bfe9703ca7b9ec0570cb6e7b5c83138
Sacha Schmid <schmid.sacha@gmail.com>
2013-03-06 21:39:05 +0100

    Revise JavaScript
    Switch from old JavaScript to a module pattern in order to make the code easier to work on.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// TODO: CHECK
+
 if (options.gaTracking) {
   var _gaq = [['_setAccount', variables.gaTrackingCode], ['_trackPageview']];
   (function (d, t) {
@@ -9,11 +11,36 @@ if (options.gaTracking) {
   }(document, 'script'));
 }
 
-var isLoading = false,
-    countwords = function (text) {
-      return text.replace(/\s+/g, ' ').split(' ').length;
-    },
-    writeWordNumber = function () {
+var Readr = (function () {
+
+  var self,
+      SWIPE_THRESHOLD = 50,
+      SWIPE_VERTICAL_THRESHOLD = 15,
+      SWIPE_CLASS = 'swiped',
+      KEYBOARD_NAVIGATION_PADDING = 20,
+      SMOOTH_SCROLLING_SPEED = 250,
+      touch = 'ontouchstart' in document.documentElement,
+      isLoading = false,
+      doc = $(document),
+      loadMore = document.getElementById('jsLoadMore'),
+      scrollTo = function (point) {
+
+        /**
+         * Scrolling through posts using keyboard navigation (j/k) can either
+         * be animated or jumping. This is set through the Customisation screen
+         * and checked via the global options object.
+         */
+
+        if (options.animatePageScrolling) {
+            $('html, body').animate({scrollTop: point}, SMOOTH_SCROLLING_SPEED);
+          } else {
+            doc.scrollTop(point);
+          }
+      };
+
+  return {
+
+    writeWordNumber: function () {
       var info = $('.post-info');
 
       if (info.length) {
@@ -27,203 +54,312 @@ var isLoading = false,
          */
 
         var text = info.prev('.text')[0],
-            words = 0;
+            words = text ? self.countwords($(text).find('.body').text()) : 0;
 
-        if (text) {
-          words = countwords($(text).find('.body').text());
-        }
+        /**
+         * Tumblr lacks a localised string for {words}. While I have already
+         * tried contacting Tumblr about lacking strings, there is hardly any
+         * chance to get them to do something about it.
+         *
+         * (see: http://cl.ly/Mar0)
+         */
 
         if (words) {
-
-          /**
-           * Tumblr lacks a localised string for {words}. While I have already
-           * tried contacting Tumblr about lacking strings, there is hardly any
-           * chance to get them to do something about it.
-           *
-           * (see: https://groups.google.com/forum/?fromgroups=#!topic/tumblr-themes/btz22uq8ZwE)
-           */
-
           info.find('ul').append('<li class="info-child"><b>Words: </b>' + words + ' words</li>');
         }
       }
     },
-    pageNavigation = function (direction) {
-      var doc = $(document),
-          scrollTop = parseInt(doc.scrollTop(), 10),
-          articles = $(document.getElementById('posts')).find('article'),
-          articleCount = articles.length,
-          i = 0;
 
-      if (1 === direction) {
-        for ( ; i < articleCount; i++) {
-          var offset = parseInt($(articles[i]).offset().top, 10);
+    countwords: function (text) {
 
-          if (offset >= scrollTop + 20) {
-            if (options.animatePageScrolling) {
-              $('html, body').animate({scrollTop: 0 < i ? parseInt($(articles[i - 1]).offset().top, 10) - 20 : 0}, 250);
-            } else {
-              doc.scrollTop(0 < i ? parseInt($(articles[i - 1]).offset().top, 10) - 20 : 0);
-            }
+      /**
+       * This is a simple function to count words on a given string of text.
+       * It is taken from helpers.js (https://github.com/RadLikeWhoa/helpers.js).
+       */
 
-            return false;
-          }
-        }
-
-        if (options.animatePageScrolling) {
-          $('html, body').animate({scrollTop: parseInt($(articles[--i]).offset().top, 10) - 20}, 250);
-        } else {
-          doc.scrollTop(parseInt($(articles[--i]).offset().top, 10) - 20);
-        }
-      } else if (2 === direction) {
-        for ( ; i < articleCount; i++) {
-          var offset = parseInt($(articles[i]).offset().top, 10);
-
-          if (offset > scrollTop + 20) {
-            if (options.animatePageScrolling) {
-              $('html, body').animate({scrollTop: offset - 20}, 250);
-            } else {
-              doc.scrollTop(offset - 20);
-            }
-
-            return false;
-          }
-        }
-
-        var loadMore = document.getElementById('jsLoadMore');
-
-        if (loadMore) {
-          if (options.animatePageScrolling) {
-            $('html, body').animate({scrollTop: parseInt($(loadMore).offset().top, 10) - 20}, 250);
-          } else {
-            doc.scrollTop(parseInt($(loadMore).offset().top, 10) - 20);
-          }
-
-          loadPosts();
-        }
-      }
+      return text.replace(/\s+/g, ' ').split(' ').length;
     },
-    loadPosts = function () {
-      if (!isLoading) {
-        isLoading = true;
 
-        var button = $(document.getElementById('jsLoadMore')),
-            href = button.attr('href'),
-            page = href.split('/').pop(),
-            newHref = href.replace(page, ++page);
+    loadPosts: function () {
 
-        button.text(localisedStrings.loading + '...');
+      /**
+       * In order to prevent the user from firing multiple request at once,
+       * check the isLoading variable if there is a request going on at the
+       * moment.
+       */
+
+      if (isLoading) return;
+      isLoading = true;
+
+      var href = loadMore.href,
+          page = href.split('/').pop();
+
+      loadMore.textContent = localisedStrings.loading + '...';
+
+      /**
+       * Make an AJAX request for the next page (based on the URL of
+       * loadMore) to fetch its posts.
+       *
+       * This approach is very similar to the one Tumblr is using on their
+       * custom mobile theme.
+       */
+
+      $.ajax({
+        url: href,
+        success: function (results) {
+          results = $(results);
+
+          var posts = results.find('#posts').html().trim();
+
+          /**
+           * If the request returned posts they are appended to the list of
+           * existing posts.
+           *
+           * Unless the request fails because of an actual error (which is
+           * handled through the error callback) there should be at least one
+           * post, otherwise the request should not have been possible.
+           */
+
+          if (posts.length > 0) {
+            $(document.getElementById('posts')).append(posts);
+
+            /**
+             * In modern browsers the window's location is updated to reflect
+             * the fetched page. This is to not break the behaviour of the back
+             * button and the browser history.
+             */
+
+            window.history.pushState(null, null, href);
+
+            /**
+             * As a global Google Analytics object exists, inline requests can be
+             * pushed so those views don't get lost.
+             */
+
+            // TODO: CHECK
+
+            if (options.gaTracking) _gaq.push(['_trackPageview', href]);
+
+            /**
+             * If the request returned a page that contains a new loadMore button,
+             * update the button's URL to reflect the next page. If not, remove the
+             * button so users can't load an inexisting page.
+             */
+
+            if (results.find('#jsLoadMore').length) {
+              loadMore.href = href.replace(page, ~~page + 1);
+              loadMore.textContent = localisedStrings.loadMore;
+            } else {
+              loadMore.parentNode.removeChild(loadMore);
+            }
+          }
+
+          isLoading = false;
+        },
+        error: function (request, status, error) {
+
+          /**
+           * Situations where the AJAX request fails are handled here.
+           * The button's text is updated to reflect the returned error
+           * and reset 3 seconds later. During this period, no additional
+           * request is possible.
+           */
+
+          loadMore.textContent = error;
+
+          setTimeout(function () {
+            loadMore.textContent = localisedStrings.loadMore;
+            isLoading = false;
+          }, 3000);
+        }
+      });
+    },
+
+    pageNavigation: function (direction) {
+      var scrollTop = doc.scrollTop(),
+          articles = $(document.getElementById('posts')).find('article'),
+          offset = 0;
+
+      if (direction === "next") {
 
         /**
-         * Make an AJAX request for the next page to fetch its posts.
-         *
-         * This approach is very similar to the one Tumblr is using on their
-         * custom mobile theme.
-         *
-         * The request is based on various steps. First, the next page is
-         * fetched and filtered for the posts. Then, the URL is update to not
-         * break the browser's back button. Also, the loaded page is tracked
-         * in Google Analytics so your stats don't get messed up. Last, the
-         * loaded posts are appended and the initiating button changes back to
-         * the appropriate state.
+         * Loop through the posts. As soon as a post is discovered having an
+         * top-offset that's larger than the current vertical scroll position,
+         * scroll to that article.
          */
 
-        $.ajax({
-          url: href,
-          success: function (results) {
-            results = $(results);
+        for (var i = 0, j = articles.length ; i < j; i++) {
+          offset = $(articles[i]).offset().top - KEYBOARD_NAVIGATION_PADDING;
 
-            var posts = results.find('#posts').html(),
-                nextLink = results.find('#jsLoadMore').length;
+          if (offset > scrollTop) {
+            scrollTo(offset);
+            break;
+          }
+        }
 
-            if ($.trim(posts).length > 0) {
-              $(document.getElementById('posts')).append(posts);
+        /**
+         * If a visitor tries to navigate down from the last post, load more
+         * posts and scroll to where they will appear.
+         */
 
-              window.history.pushState(null, null, href);
-              if (options.gaTracking) {
-                _gaq.push(['_trackPageview', href]);
-              }
+        if (loadMore) {
+          scrollTo($(loadMore).offset().top - KEYBOARD_NAVIGATION_PADDING);
+          self.loadPosts();
+        }
+      } else if (direction === "prev") {
 
-              if (nextLink) {
-                button.attr('href', newHref).text(localisedStrings.loadMore);
-              } else {
-                button.remove();
-              }
-            }
+        /**
+         * Loop through the posts. As soon as a post is discovered having an
+         * offset that is higher than the current scroll position, scroll to
+         * the post before it.
+         */
 
-            isLoading = false;
-          },
-          error: function (request, status, error) {
-            button.text(error);
-            setTimeout(function () {
-              button.text(localisedStrings.loadMore);
-            }, 3000);
+        for (var i = 0, j = articles.length ; i < j; i++) {
+          offset = $(articles[i]).offset().top - KEYBOARD_NAVIGATION_PADDING;
+
+          if (offset >= scrollTop) {
+            scrollTo(0 < i ? $(articles[i - 1]).offset().top : 0);
+            return;
+          }
+        }
+
+        /**
+         * If no posts matching the criteria are found, scroll to the
+         * penultimate post.
+         */
+
+        scrollTo($(articles[--i]).offset().top - KEYBOARD_NAVIGATION_PADDING);
+      }
+    },
+
+    bindUIActions: function () {
+
+      /**
+       * If keyboard navigation is enabled on the Customisation screen, bind
+       * the necessary events to the window.
+       */
+
+      if (options.keyboardNav) {
+        $(window).on('keyup', function (e) {
+          var code = e.keyCode;
+
+          /**
+           * j (74) and arrow right (39) navigate to the next post, k (75) and
+           * arrow left (37) navigate to the previous post.
+           */
+
+          if (39 === code || 74 === code) {
+            self.pageNavigation("next");
+          } else if (37 === code || 75 === code) {
+            self.pageNavigation("prev");
           }
         });
       }
-    };
 
-$(function() {
-  var loadMore = document.getElementById('jsLoadMore'),
-      touch = 'ontouchstart' in document.documentElement;
+      /**
+       * If there is a next page and inline post loading is enabled, bind the
+       * requesting action to the button.
+       */
 
-  if (touch) {
-    $(document.body).addClass('touch');
-
-    var swipeStart, vertStart;
-
-    $('.posts').on('touchstart', 'article', function (e) {
-      e = e.originalEvent;
-
-      if (e.touches.length === 1) {
-        var touch = e.touches[0];
-        swipeStart = touch.pageX;
-        vertStart = touch.pageY;
+      if (loadMore) {
+        $(loadMore).on('click', function (e) {
+          e.preventDefault();
+          self.loadPosts();
+        });
       }
-    });
+    },
 
-    $('.posts').on('touchend', 'article', function (e) {
-      e = e.originalEvent;
-      var touches = e.changedTouches,
-          $this = $(this);
+    bindTouchActions: function () {
+      var swipeStart, vertStart;
 
-      if (touches.length == 1 && touches[0].pageY > vertStart - 15 && touches[0].pageY < vertStart + 15) {
-        if (touches[0].pageX - swipeStart < -50 && !$this.hasClass('swiped')) {
-          var old = $('.swiped');
+      /**
+       * As soon as a post is touched, the coordinates get logged to later
+       * calculate the gesture.
+       *
+       * swipeStart is used for the horizontal offset, while vertStart is
+       * used to detect simple scrolling.
+       */
 
-          if (old.length) {
-            old.removeClass('swiped');
-          }
+      $('.posts').on('touchstart', 'article', function (e) {
+        e = e.originalEvent;
 
-          $this.addClass('swiped');
-        } else if (touches[0].pageX - swipeStart > 50) {
-          $this.removeClass('swiped');
+        if (e.touches.length === 1) {
+          var touch = e.touches[0];
+          swipeStart = touch.pageX;
+          vertStart = touch.pageY;
         }
-      } else {
-        e.preventDefault();
-      }
-    });
-  } else {
-    $(document.body).addClass('no-touch');
-  }
+      });
 
-  if (options.keyboardNav) {
-    $(window).on('keyup', function (e) {
-      var code = e.keyCode;
+      /**
+       * Once the finger is released from a post, check if the gesture is a
+       * swipe. If it is, trigger the meta revealing animation.
+       */
 
-      if (37 === code || 75 === code) {
-        pageNavigation(1);
-      } else if (39 === code || 74 === code) {
-        pageNavigation(2);
-      }
-    });
-  }
+      $('.posts').on('touchend', 'article', function (e) {
 
-  if (loadMore) {
-    $(loadMore).on('click', function (e) {
-      e.preventDefault();
-      loadPosts();
-    });
-  }
+        /**
+         * e.originalEvent is required because jQuery behaves weirdly with
+         * touch events and doesn't send the correct event.
+         */
 
-  writeWordNumber();
-});
+        e = e.originalEvent;
+
+        var touches = e.changedTouches,
+            $this = $(this);
+
+        /**
+         * Check if the swipe was indeed a swipe and not basic scrolling.
+         */
+
+        if (touches.length === 1 && touches[0].pageY > vertStart - SWIPE_VERTICAL_THRESHOLD && touches[0].pageY < vertStart + SWIPE_VERTICAL_THRESHOLD) {
+          if (touches[0].pageX - swipeStart < -SWIPE_THRESHOLD && !$this.hasClass(SWIPE_CLASS)) {
+
+            /**
+             * If the swipe was to the left and over the swipe distance
+             * threshold, reveal the swiped post's meta and move the
+             * previously swiped post back to its initial position.
+             */
+
+            var old = $('.' + SWIPE_CLASS);
+            if (old.length) old.removeClass(SWIPE_CLASS);
+            $this.addClass(SWIPE_CLASS);
+          } else if (touches[0].pageX - swipeStart > SWIPE_THRESHOLD) {
+
+            /**
+             * If the swipe was to the right and over the swipe distance
+             * threshold, move the post back to its initial position.
+             */
+
+            $this.removeClass(SWIPE_CLASS);
+          }
+        } else {
+
+          /**
+           * Don't do anything if the user has scrolled through the list of
+           * posts.
+           */
+
+          e.preventDefault();
+        }
+      });
+    },
+
+    init: function () {
+      self = this;
+
+      /**
+       * Call the functions required to enable all functionality at launch.
+       * Mainly, this means binding events to elements. If the visitor uses
+       * a touch screen devices, special touch events are attached as well.
+       */
+
+      self.writeWordNumber();
+      self.bindUIActions();
+      if (touch) self.bindTouchActions();
+    }
+
+  };
+
+}());
+
+Readr.init();
